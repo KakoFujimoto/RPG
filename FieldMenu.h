8928cc14ce99a457e3d93a736b5b1f82d302e72c
kitamoo <ayaka20111005@yahoo.co.jp>
2025-11-04 21:41:50 +0900

    アイテムを拾ってメニュー上で表示できるようになるまで

--- FieldMenu.h
+++ FieldMenu.h
@@ -22,8 +22,6 @@ private:
 	int selectedIndex = 0;
 	GameManager* gm;
 
-	IMenu* submenu;
-
 public:
 	FieldMenu(GameManager* gm);
 	void choose();
@@ -36,43 +34,5 @@ public:
 	void drawItemList();
 	void drawParameter();
 	void drawSpellList();
-	
 
-};
-
-
-
-
-/** 新しく追加したインターフェース -> 各メニューの抽象化(ルート、どうぐ、呪文など) */
-class IMenu {
-public:
-	virtual void update() = 0;
-	virtual void draw(Display& display) = 0;
-};
-
-
-/** FieldMenu */
-class FieldMenu2 {
-public:
-	void open();
-
-	void draw()
-	{
-		std::for_each(menus.begin(), menus.end(), [](auto& item) {
-			item.draw();
-		});
-	}
-
-	void update()
-	{
-		if (menus.size() > 0) {
-			auto& lastMenu = menus[menus.size() - 1];
-			lastMenu.update();
-		}
-	}
-
-	void addMenu(IMenu* menu) { menus.push_back(menu); }
-
-private:
-	std::vector<std::shared_ptr<IMenu>> menus;
 };
